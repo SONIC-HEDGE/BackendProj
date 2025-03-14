2138e6e8c32e1381a3bafd008ca3308794242785
SLASH-PL <hubekhaladyna@gmail.com>
2025-03-14 14:37:48 +0100

    commit post

---
+++ BackendProj/Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace YourNamespace.Controllers
+{
+    public class HelloController : Controller
+    {
+        [HttpGet("/hello")]
+        public IActionResult Hello()
+        {
+            return Content("Hello World!");
+        }
+
+        [HttpPut("/put")]
+        public IActionResult SetVariable([FromQuery] string parameter)
+        {
+            Console.WriteLine(parameter);
+            return Ok($"Received: {parameter}");
+        }
+        
+        [HttpGet("/status")]
+        public IActionResult Status()
+        {
+            return Ok("API is running smoothly!");
+        }
+
+    }
+}
